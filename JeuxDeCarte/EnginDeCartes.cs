91ebfca939fc1055e84058dadd2acc68c6c9b3b9
‘RoucherChristophe’ <c.roucher@orange.fr>
2018-11-22 09:30:14 +0100

    création de la classe 'EnginDeCartes' avec 2 fonctions créer et brassées les cartes

---
+++ JeuxDeCarte/EnginDeCartes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuxDeCarte
+{
+    public class EnginDeCartes
+    {
+        // fonction qui créer un jeux de cartes
+        public List<string> RetournerNouveauPaquet()
+        {
+            // création du jeux de cartes
+            // list dynamique
+            List<string> Cartes = new List<string>();
+
+            List<string> typesCartes = new List<string>() { "coeur", "carreau", "piques", "trefle" };
+
+            for (int typeCarte = 0; typeCarte < 4; typeCarte++)
+            {
+                Cartes.Add(string.Format("As de {0}", typesCartes[typeCarte]));
+
+                for (int i = 2; i < 11; i++)
+                {
+                    Cartes.Add(string.Format("{0} de {1}", i, typesCartes[typeCarte]));
+                }
+
+                Cartes.Add(string.Format("Valet de {0}", typesCartes[typeCarte]));
+                Cartes.Add(string.Format("Dame de {0}", typesCartes[typeCarte]));
+                Cartes.Add(string.Format("Roi de {0}", typesCartes[typeCarte]));
+            }
+            // on retourne les cartes
+            return Cartes;
+        }
+
+
+        // on créer une fonction pour brasséer les cartes
+        // c'est une liste de type string qui reçoit en parametre une list string que l'on appelle Cartes
+        public List<string> BrassCarte(List<string> Cartes)
+        {
+            List<string> CartesBrassees = new List<string>();
+            // on créer un nombre aléatoire
+            Random rnd = new Random();
+
+            // tant que CarteBrassees est inférieur à 52
+            while (CartesBrassees.Count < 52)
+            {
+                int i = rnd.Next(0, 52); // on obtient un chiffre entre 0 et 52
+                // si CarteBrassees ne contient pas la carte sélectionnée, on l'ajoute au paquet de CarteBrassees
+                if (!CartesBrassees.Contains(Cartes[i]))
+                {
+                    CartesBrassees.Add(Cartes[i]);
+                }
+            }
+
+            return CartesBrassees;
+        }
+
+
+
+
+    }
+}
--- JeuxDeCarte/Program.cs
+++ JeuxDeCarte/Program.cs
@@ -10,52 +10,23 @@ namespace JeuxDeCarte
     {
         static void Main(string[] args)
         {
+            //on creer l'instance d'EnginDeCartes
+            EnginDeCartes engin = new EnginDeCartes();
 
-            // création du jeux de cartes
-            // list dynamique
-            List<string> Cartes = new List<string>();
-
-            List<string> typesCartes = new List<string>() { "coeur", "carreau", "piques", "trefle" };
-
-            for (int typeCarte = 0; typeCarte < 4; typeCarte++)
-            {
-                Cartes.Add(string.Format("As de {0}", typesCartes[typeCarte]));
-
-                for (int i = 2; i < 11; i++)
-                {
-                    Cartes.Add(string.Format("{0} de {1}", i, typesCartes[typeCarte]));
-                }
-
-                Cartes.Add(string.Format("Valet de {0}", typesCartes[typeCarte]));
-                Cartes.Add(string.Format("Dame de {0}", typesCartes[typeCarte]));
-                Cartes.Add(string.Format("Roi de {0}", typesCartes[typeCarte]));
-            }
-
+            // Cartes reçoit le nouveau paquet de cartes
+            List<string> Cartes = engin.RetournerNouveauPaquet();
 
             // création du jeux de cartes brassées
-            // List dynamique
+            // CarteBrassees reçoit une liste demandé à l'engin BrassCarte avec en parametre un nouveau paquet de carte
 
-            List<string> CartesBrassees = new List<string>();
-            // on créer un nombre aléatoire
-            Random rnd = new Random();
-
-            // tant que CarteBrassees est inférieur à 52
-            while (CartesBrassees.Count < 52)
-            {
-                int i = rnd.Next(0, 52); // on obtient un chiffre entre 0 et 52
-                // si CarteBrassees ne contient pas la carte sélectionnée, on l'ajoute au paquet de CarteBrassees
-                if (!CartesBrassees.Contains(Cartes[i]))
-                {
-                    CartesBrassees.Add(Cartes[i]);
-                }
-            }
+            List<string> CartesBrassees = engin.BrassCarte(Cartes);
+           
 
             for (int i = 0; i < 52; i++)
             {
                 Console.WriteLine(CartesBrassees[i]);
                 Console.ReadLine();
             }
-
 
             }
         }
